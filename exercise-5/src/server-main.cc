a8fa8ba85064fd9e9ea388d9e6a53f01bc41c4e0
Manogya Singh Suryansh <manogyasinghsuryansh@gmail.com>
2025-06-02 18:02:21 +0000

    remove unused imports from server-main.cc

--- exercise-5/src/server-main.cc
+++ exercise-5/src/server-main.cc
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 #include "endpoints/Server.h"
 
 int main() {
-  namespace ttc = tt::chat;
   const int kPort = 8080;
-  ttc::server::Server thisserverobject(kPort);
+
+  std::cout << "Starting Server...\n";
+  tt::chat::server::Server thisserver(kPort);
+
   return 0;
 }
