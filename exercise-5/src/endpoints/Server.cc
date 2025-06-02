356c74c24aec066f936f3b0b112b6f7c9337f9f1
Manogya Singh Suryansh <manogyasinghsuryansh@gmail.com>
2025-06-02 18:36:06 +0000

    use initialiser list for Server class constructor

--- exercise-5/src/endpoints/Server.cc
+++ exercise-5/src/endpoints/Server.cc
@@ -2,13 +2,12 @@
 #include "../net/chat-sockets.h"
 
 namespace tt::chat::server {
-Server::Server(int port) {
-  port_ = port;
-  server_socket_fd_ = create_server_socket();
-  address_ = create_server_address();
+Server::Server(int port)
+    : port_(port), server_socket_fd_(create_server_socket()),
+      address_(create_server_address()) {
   bind_address_to_socket();
-  std::cout << "Server listening on port " << port_ << "\n";
   listen_on_socket();
+  std::cout << "Server listening on port " << port_ << "\n";
   handle_connections();
 }
 
@@ -27,7 +26,8 @@ int Server::create_server_socket() {
 }
 
 void Server::bind_address_to_socket() {
-  auto err_code = bind(server_socket_fd_, (sockaddr *)&address_, sizeof(address_));
+  auto err_code =
+      bind(server_socket_fd_, (sockaddr *)&address_, sizeof(address_));
   tt::chat::check_error(err_code < 0, "bind failed\n");
 }
 
@@ -41,8 +41,8 @@ void Server::handle_accept(int client_socket) {
   namespace ttc = tt::chat;
   char buffer[kBufferSize] = {0};
   ssize_t read_size = read(client_socket, buffer, kBufferSize);
-  ttc::check_error(read_size < 0,
-                   "Read error on client socket " + std::to_string(client_socket));
+  ttc::check_error(read_size < 0, "Read error on client socket " +
+                                      std::to_string(client_socket));
   if (read_size > 0) {
     std::cout << "Received:" << buffer << "\n";
     send(client_socket, buffer, read_size, 0);
@@ -64,9 +64,9 @@ sockaddr_in Server::create_server_address() {
 void Server::handle_connections() {
   socklen_t address_size = sizeof(address_);
   while (true) {
-    int client_socket = accept(server_socket_fd_, (sockaddr *)&address_, &address_size);
-    check_error(client_socket < 0,
-                "Couldn't accept the connected socket.\n");
+    int client_socket =
+        accept(server_socket_fd_, (sockaddr *)&address_, &address_size);
+    check_error(client_socket < 0, "Couldn't accept the connected socket.\n");
     handle_accept(client_socket);
   }
 }
