9d772055160960f87d0103cbc45f282cc4ba931d
Manogya Singh Suryansh <manogyasinghsuryansh@gmail.com>
2025-06-02 19:31:16 +0000

    client class: encapsulate utility functions

--- exercise-5/src/endpoints/Client.h
+++ exercise-5/src/endpoints/Client.h
@@ -1,24 +1,23 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
-#include "../utils.h"
 #include "../net/chat-sockets.h"
+#include "../utils.h"
 #include <netinet/in.h>
 #include <string>
 
 class Client {
 public:
-  Client (std::string server_address, int port);
+  Client(std::string server_address, int port);
   ~Client();
+  void send_and_receive_message(const std::string &message);
+
+private:
   sockaddr_in server_address_;
   int socket_fd_;
-
+  void connect_to_server();
   static sockaddr_in create_server_address(const std::string &server_ip,
                                            int port);
-
-  void connect_to_server();
-
-  void send_and_receive_message(const std::string &message);
 };
 
 #endif
