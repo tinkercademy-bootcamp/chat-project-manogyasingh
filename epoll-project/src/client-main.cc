22fb532a1ce61f927ae13a8d727919167fe75a42
manogyasingh <manogyasinghsuryansh@gmail.com>
2025-06-04 09:58:15 +0530

    remove duplicate imports, remove epoll tutorial

--- epoll-project/src/client-main.cc
+++ epoll-project/src/client-main.cc
@@ -1,10 +1,5 @@
-#include <arpa/inet.h>
 #include <iostream>
-#include <netinet/in.h>
 #include <string>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 #include "client/chat-client.h"
 
--- epoll-project/src/server-main.cc
+++ epoll-project/src/server-main.cc
@@ -1,13 +1,7 @@
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <unistd.h>
-
 #include "server/chat-server.h"
 
 int main() {
   const int kPort = 8080;
-
   tt::chat::server::Server server(kPort);
   server.handle_connections();
 
