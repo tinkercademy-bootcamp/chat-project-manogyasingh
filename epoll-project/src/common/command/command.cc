64f85166499b7612cc88d00335c6e2bb4a7ec0d8
manogyasingh <manogyasinghsuryansh@gmail.com>
2025-06-06 08:54:26 +0530

    commandparser add helper funcs

--- epoll-project/src/common/command/command.cc
+++ epoll-project/src/common/command/command.cc
@@ -3,7 +3,23 @@
 #include <cctype>
 #include <string_view>
 
-namespace {}
+
+namespace {
+using std::string_view;
+string_view ltrim(string_view s) {
+  while (!s.empty() && std::isspace(static_cast<unsigned char>(s.front())))
+    s.remove_prefix(1);
+  return s;
+}
+
+string_view token(string_view& s) {
+  s = ltrim(s);
+  auto pos = s.find_first_of(" \t\r\n");
+  auto head = s.substr(0, pos);
+  s = (pos == string_view::npos) ? string_view{} : s.substr(pos);
+  return head;
+}
+}  // namespace
 
 namespace xtc::command {
 std::optional<Command> parse_line(std::string_view line) {
